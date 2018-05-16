976834189e1993003a517908ea4010ba2fd1013a
dell <dell@RAHUL-PC>
2018-05-16 20:02:41 +0530

    16-05-2018 20:02

---
+++ UtilityNg/UtilityNg/Models/CustomerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UtilityNg.Models
+{   [Table("customer")]
+    public class CustomerModel
+    {   [Key]
+        public int AutoId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        [Required]
+        public int Age { get; set; }
+        public int Phone { get; set; }
+    }
+}
---
+++ UtilityNg/UtilityNg/Models/SampleDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace UtilityNg.Models
+{
+    public class SampleDataContext:DbContext
+    {
+        public SampleDataContext():base("name=dbcs")
+        {
+            Database.SetInitializer<SampleDataContext>(null);
+        }
+        public virtual DbSet<CustomerModel> Customer { get; set; }
+    }
+}
